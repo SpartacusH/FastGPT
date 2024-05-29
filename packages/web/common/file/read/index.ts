b5f2cc4ad01d277849d8a839b1c24df9b38db570
echo233 <936560751@qq.com>
2024-05-29 14:53:02 +0800

    支持多类型文件

--- packages/web/common/file/read/index.ts
+++ packages/web/common/file/read/index.ts
@@ -3,7 +3,7 @@ import { readCsvContent } from './csv';
 import { readHtmlFile } from './html';
 import { readMdFile } from './md';
 import { readPdfFile } from './pdf';
-import { readDocContent} from './doc';
+import { readDocContent } from './doc';
 import { readFileRawText } from './rawText';
 import { readWordFile } from './word';
 
@@ -20,6 +20,8 @@ export const readFileRawContent = async ({
 
   switch (extension) {
     case 'txt':
+      console.log('read txt file');
+
       return readFileRawText(file);
     case 'md':
       return readMdFile({
@@ -33,19 +35,22 @@ export const readFileRawContent = async ({
       });
     case 'csv':
       return readCsvContent({ file });
+    // case 'pdf':
+    //   const pdf = await loadFile2Buffer({ file });
+    //   // 判断是否是扫描版pdf的方法
+    //   return readPdfFile({ pdf });
     case 'pdf':
-      const pdf = await loadFile2Buffer({ file });
-      return readPdfFile({ pdf });
     case 'ofd':
     case 'doc':
     case 'wps':
+    case 'docx':
       // @ts-ignore
       return readDocContent({ file });
-    case 'docx':
-      return readWordFile({
-        file,
-        uploadImgController: uploadBase64Controller
-      });
+    // case 'docx':
+    //   return readWordFile({
+    //     file,
+    //     uploadImgController: uploadBase64Controller
+    //   });
 
     default:
       return {
